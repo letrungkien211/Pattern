8046a35e0cf7050e06b29da7867eaffc5be17b35
letrungkien211 <letrungkien211@gmail.com>
2013-02-16 22:16:35 +0700

    Done 4th

---
+++ auto/report.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook "report"
+ (lambda ()
+    (TeX-run-style-hooks
+     "graphicx"
+     "setspace"
+     "hyperref"
+     "url"
+     "color"
+     "listings"
+     "palatino"
+     "geometry"
+     "10pt}"
+     "includefoot"
+     "latex2e"
+     "art11"
+     "article"
+     "a4paper"
+     "11pt")))
+
